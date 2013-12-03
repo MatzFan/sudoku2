86c6e8230a3d2a0220b9ea13eaa4e761c1c00b89
Bruce Steedman <bruce.steedman@gmail.com>
2013-12-03 13:10:16 +0000

    Added knowledge of neighbouring cell refs to Cell class

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,10 +4,25 @@ class Cell
 
   include Constraints
 
+  attr_reader :ref
+  attr_accessor :values
+
   def initialize(args)
-    ref = args[:ref]
+    @ref = args[:ref]
     value = args[:value]
     value == 0 || !value ? @values = (1..9).to_a : @values = [args[:value]]
+  end
+
+  def neighbour_cells
+
+  end
+
+  def constraint_cell_refs
+    all_cells_in_constraint_sets.flatten.uniq.sort.reject { |e| e == ref }
+  end
+
+  def all_cells_in_constraint_sets
+    CELL_SETS[ref].map { |set| CONSTRAINT_SETS[set] }
   end
 
   def solved?
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,21 +3,30 @@ require 'cell'
 describe Cell do
   context 'initialization' do
 
-    let(:cell_1) { Cell.new({ref: 1, value: 0}) }
-    let(:cell_9) { Cell.new({ref: 9, value: 7}) }
-    let(:cell_31) { Cell.new({ref: 31, value: 6}) }
+    let(:cell_0) { Cell.new({ref: 0, value: 0}) }
+    let(:cell_0_constraint_refs) {[1,2,3,4,5,6,7,8,9,10,11,18,19,20,27,36,45,54,63,72]}
+    let(:cell_8) { Cell.new({ref: 8, value: 7}) }
+    let(:cell_30) { Cell.new({ref: 30, value: 6}) }
 
 
     it 'should know it is not solved' do
-      expect(cell_31).to be_solved
+      expect(cell_30).to be_solved
     end
 
     it 'should know when it is solved' do
-      expect(cell_1).not_to be_solved
+      expect(cell_0).not_to be_solved
     end
 
-    it 'should know when its values are constrined by a cell in same row' do
-      expect(cell_1.values).to eq([1,2,3,4,5,6,8,9]) # constraiend by cell 9
+    it 'should know the the range of values it may have when initialized' do
+      cell_0.values.should == [1,2,3,4,5,6,7,8,9]
+    end
+
+    it 'should know the references of all cells constraining its value' do
+      cell_0.constraint_cell_refs.should == cell_0_constraint_refs
+    end
+
+    it 'should know the the range of values it may have when initialized' do
+      cell_0.values.should == [1,2,3,4,5,6,7,8,9]
     end
 
   end # of context
