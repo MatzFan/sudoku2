5f74eb3bbe4de3e4c5bd57b35e99e30daaa0053e
Bruce Steedman <bruce.steedman@gmail.com>
2013-12-10 16:40:55 +0000

    Refactored to have cells solve themselves if absolutely constrained by their neighbours

--- lib/cell.rb
+++ lib/cell.rb
@@ -7,20 +7,30 @@ class Cell
     @values = (1..9).to_a
   end
 
+  def setup_neighbours(neighbours)
+    @neighbours = neighbours
+  end
+
   def value=(value) # for initialization only - takes integer arg
     @values = [value] if value != 0
+  end
+
+  def update_values
+    @neighbours.each do |other_cell|
+      reduce_values(other_cell) if other_cell.solved?
+    end
   end
 
   def solved?
     @values.length == 1
   end
 
-  def reduce_cell_values(solved_cell)
+  def reduce_values(solved_cell)
     @values -= solved_cell.values
   end
 
   def to_s
-    values.length == 1 ? values[0].to_s : '0'
+    values.length == 1 ? values[0].to_s : '-'
   end
 
 end
--- lib/grid.rb
+++ lib/grid.rb
@@ -3,17 +3,15 @@ require_relative 'constraints'
 
 class Grid
 
-  attr_reader :cells, :cell_constraints
+  attr_reader :cells
 
   include Constraints
 
   def initialize(puzzle)
     raise ArgumentError, 'Not 81 cells' if puzzle.length != 81
     raise ArgumentError, 'Non digits' if !(puzzle =~ /\d{81}/)
-    # @cells = Array.new(9) { Array.new(9) { Cell.new } }
     @cells = (0..80).inject([]) { |arr, ref| arr << Cell.new(ref) }
-    @cell_constraints = Array.new(81) { Array.new } # array of cells
-    setup_cell_constraint_sets
+    setup_cell_neighbours
     feed_in_puzzle_values(puzzle.split(''))
   end
 
@@ -22,53 +20,31 @@ class Grid
     update_cell_values
   end
 
-  def unsolved_cells
-    unsolved_cell_refs.map { |ref| cell_at(ref)}
-  end
-
-  def unsolved_cell_refs
-    (0..80).reject { |n| cell_at(n).solved? }
-  end
-
-  def solved_cells(cells)
+  def solved_cells
     cells.reject { |cell| !cell.solved? }
   end
 
+  def unsolved_cells
+    cells.reject { |cell| cell.solved? }
+  end
+
   def update_cell_values
-    unsolved_cell_refs.each do |ref|
-      this_cell = cell_at(ref)
-      other_solved_cells = solved_cells(cell_constraints[ref])
-      other_solved_cells.each do |other_solved_cell|
-        this_cell.reduce_cell_values(other_solved_cell)
-      end
-    end
+    unsolved_cells.each { |cell| cell.update_values }
   end
 
   def feed_in_puzzle_values(puzzle_array)
     (0..80).each { |n| cells[n].value = puzzle_array[n].to_i }
   end
 
-  def cell_at(num)
-    # cells[num / 9][num % 9]
-    cells[num]
-  end
-
-  def setup_cell_constraint_sets
-    (0..80).each do |n|
-      constraint_cell_refs(n).each { |ref| @cell_constraints[n] << cell_at(ref) }
+  def setup_cell_neighbours
+    cells.each do |cell|
+      neighbours = CELL_CONSTRAINTS[cell.ref].inject([]) { |arr, ref| arr << cells[ref]}
+      cell.setup_neighbours(neighbours)
     end
   end
 
   def solved?
-    solved_cells(cells.flatten).count == 81
-  end
-
-  def constraint_cell_refs(ref)
-    constraint_sets_for(ref).flatten.uniq.sort.reject { |e| e == ref }
-  end
-
-  def constraint_sets_for(ref)
-    CELL_SETS[ref].map { |set| CONSTRAINT_SETS[set] }
+    solved_cells(cells).count == 81
   end
 
   def to_simple_string
@@ -76,7 +52,8 @@ class Grid
   end
 
   def to_s
-    # cells.map { |row| row.map { |cell| sprintf("%2s", cell.to_s) }.join }.join("\n")+"\n\n"
+    rows = to_simple_string.scan(/.{9}/)
+    rows.map { |row| sprintf("%2s", row.split('')) }.join("\n")+"\n\n"
   end
 
 end # of class
@@ -86,5 +63,5 @@ g = Grid.new(s)
 # # # p g.unsolved_cell_refs.count
 # # # (0..80).each { |n| p g.cell_at(n).values }
 # # p g.solved_cells( g.cells.flatten )
-# g.solve
-# puts g
+g.solve
+puts g
